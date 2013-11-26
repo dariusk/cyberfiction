c3d39bd7c5e865392332b3f80800113c6564b7c2
Darius Kazemi <darius.kazemi@gmail.com>
2013-11-26 15:59:08 -0500

    first

---
+++ README.md
@@ -0,0 +1,10 @@
+# cyber (Cyberpunk Fiction Machine)
+
+Making cyberpunk fiction.
+
+## External licenses
+_This uses Ryan Schuft's [inflection.js](https://code.google.com/p/inflection-js/) which gracefully pluralizes nouns (among other things). inflection.js is covered by the license and copyright at the top of its file._
+
+## License
+Copyright (c) 2013 Darius Kazemi  
+Licensed under the MIT license.
---
+++ js/app.js
@@ -0,0 +1,42 @@
+/* global sharing */
+var nouns = [],
+    verbs = [];
+
+function generate(passage) {
+  passage = passage || $('#input').val().replace(/"/g,'');
+  var words = new Lexer().lex(passage);
+  var taggedWords = new POSTagger().tag(words);
+  var result = '';
+  for (i in taggedWords) {
+      var taggedWord = taggedWords[i];
+      var word = taggedWord[0];
+      var tag = taggedWord[1];
+      console.log(word + " /" + tag);  // if running in a browser, try document.writeln() instead of print() 
+      if ((tag === 'NN' || tag === 'NNS') && !word.match(/\W/) && word.length > 4) {
+        word = 'cyber' + word;
+      }
+      result += word + ' ';
+  }
+  result = result.replace(/ ,/g,',').replace(/ \./g,'.').replace(/ \!/g,'!');
+  console.log(result);
+  var generatedText = result;
+  var sharedText = passage;
+  $('#content').html(generatedText);
+  var shareUrl = window.location.href.split('?')[0]+'?passage='+encodeURIComponent(passage);
+  $('#share').attr('href', shareUrl);
+  $('.twitter-share-button').remove();
+  $('#twitterShare').html('<a href="https://twitter.com/share" class="twitter-share-button" data-url="' + shareUrl + '" data-text="' + generatedText.substr(0,110) + '..." data-lang="en">Tweet</a>');
+  if (twttr.widgets) {
+    twttr.widgets.load();
+  }
+}
+
+
+$('#generate').click(function() { generate(); });
+if (sharing.gup('passage') === '') {
+  generate();
+}
+else {
+  $('#input').val(decodeURIComponent(sharing.gup('passage')));
+  generate(decodeURIComponent(sharing.gup('passage')));
+}
